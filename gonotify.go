93ea043d4dd461279fc94b240c17afb123600e56
cbarGit <antisenso@gmail.com>
2017-01-05 14:53:11 +0100

    Changed some names (go lang better code)

--- gonotify.go
+++ gonotify.go
@@ -19,58 +19,58 @@ type wdesc struct {
 }
 
 /* inotify object struct */
-type watch_struct struct {
-	fd         int
-	watch_list map[string]*wdesc
-	objects    map[uint32]string
+type watchStruct struct {
+	fd        int
+	watchList map[string]*wdesc
+	objects   map[uint32]string
 }
 
 /* inotify constants array */
-var event_type = [7]uint32{unix.IN_CREATE, unix.IN_DELETE, unix.IN_MODIFY,
+var eventType = [7]uint32{unix.IN_CREATE, unix.IN_DELETE, unix.IN_MODIFY,
 	unix.IN_MOVED_FROM, unix.IN_MOVED_TO, unix.IN_MOVE_SELF,
 	unix.IN_DELETE_SELF}
 
 /* IS_DIR constant */
-const file_type uint32 = unix.IN_ISDIR
+const fileType uint32 = unix.IN_ISDIR
 
 /* creates an inotify object and call readEvent on that object. */
-func create_watch() (*watch_struct, error) {
+func createWatch() (*watchStruct, error) {
 
 	fd, err := unix.InotifyInit()
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	inotify_object := &watch_struct{
-		fd:         fd,
-		watch_list: make(map[string]*wdesc),
-		objects:    make(map[uint32]string),
+	inotifyObject := &watchStruct{
+		fd:        fd,
+		watchList: make(map[string]*wdesc),
+		objects:   make(map[uint32]string),
 	}
 
-	go inotify_object.readEvent()
+	go inotifyObject.readEvent()
 
-	return inotify_object, nil
+	return inotifyObject, nil
 
 }
 
-/* add path to watch_list */
-func (inotify_object *watch_struct) add_watch(d string) uint32 {
-	wd, err := unix.InotifyAddWatch(inotify_object.fd, d, unix.IN_ALL_EVENTS)
+/* add path to watchList */
+func (inotifyObject *watchStruct) addWatch(d string) uint32 {
+	wd, err := unix.InotifyAddWatch(inotifyObject.fd, d, unix.IN_ALL_EVENTS)
 	if err != nil {
 		fmt.Println(err)
 	}
-	inotify_object.watch_list[d] = &wdesc{wd: uint32(wd)}
-	inotify_object.objects[uint32(wd)] = d
+	inotifyObject.watchList[d] = &wdesc{wd: uint32(wd)}
+	inotifyObject.objects[uint32(wd)] = d
 	return uint32(wd)
 }
 
-/* remove path from watch_list */
-func (inotify_object *watch_struct) rm_watch(d string) error {
-	desc, ok := inotify_object.watch_list[d]
+/* remove path from watchList */
+func (inotifyObject *watchStruct) rmWatch(d string) error {
+	desc, ok := inotifyObject.watchList[d]
 	if !ok {
-		fmt.Println("error: cannot remove watch from watch_list")
+		fmt.Println("error: cannot remove watch from watchList")
 	}
-	wd, err := unix.InotifyRmWatch(inotify_object.fd, desc.wd)
+	wd, err := unix.InotifyRmWatch(inotifyObject.fd, desc.wd)
 	if wd == -1 {
 		fmt.Println(err)
 	}
@@ -78,14 +78,14 @@ func (inotify_object *watch_struct) rm_watch(d string) error {
 }
 
 /* read events from buffer and eventually call detectEvent */
-func (inotify_object *watch_struct) readEvent() {
+func (inotifyObject *watchStruct) readEvent() {
 	var (
 		p [unix.SizeofInotifyEvent * 4096]byte
 		i uint32
 	)
 
 	for {
-		length, err := unix.Read(inotify_object.fd, p[:])
+		length, err := unix.Read(inotifyObject.fd, p[:])
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -99,8 +99,8 @@ func (inotify_object *watch_struct) readEvent() {
 			lenN := uint32(event.Len)
 
 			if lenN != 0 {
-				path := inotify_object.objects[wd]
-				detectEvent(inotify_object, wd, path, mask, event_type[:], i, p[:], lenN)
+				path := inotifyObject.objects[wd]
+				detectEvent(inotifyObject, wd, path, mask, eventType[:], i, p[:], lenN)
 
 			}
 			i += unix.SizeofInotifyEvent + lenN
@@ -109,26 +109,26 @@ func (inotify_object *watch_struct) readEvent() {
 }
 
 /* detect the event, then print on terminal and call noty for libnotify notification */
-func detectEvent(inotify_object *watch_struct, wd uint32, path string, mask uint32, event_type []uint32, i uint32, p []byte, lenN uint32) {
+func detectEvent(inotifyObject *watchStruct, wd uint32, path string, mask uint32, eventType []uint32, i uint32, p []byte, lenN uint32) {
 	//fmt.Println(path)
 	bytes := (*[unix.PathMax]byte)(unsafe.Pointer(
 		&p[i+unix.SizeofInotifyEvent]))
 	evName := strings.TrimRight(string(bytes[0:lenN]), "\000")
-	if mask&event_type[0] != 0 {
+	if mask&eventType[0] != 0 {
 		totName := path + "/" + evName
-		if mask&file_type != 0 {
+		if mask&fileType != 0 {
 			fmt.Printf("New dir %v has been created in %v\n", evName, path)
-			inotify_object.add_watch(totName)
+			inotifyObject.addWatch(totName)
 			noty(evName, "null", path, 0, 0)
 		} else {
 			fmt.Printf("New file %v has been created in %v\n", evName, path)
-			inotify_object.add_watch(totName)
+			inotifyObject.addWatch(totName)
 			noty(evName, "null", path, 1, 0)
 		}
 	}
-	if (mask&event_type[1] != 0) ||
-		(mask&event_type[6] != 0) {
-		if mask&file_type != 0 {
+	if (mask&eventType[1] != 0) ||
+		(mask&eventType[6] != 0) {
+		if mask&fileType != 0 {
 			fmt.Printf("Dir %v has been deleted in %v\n", evName, path)
 			noty(evName, "null", path, 0, 1)
 		} else {
@@ -136,9 +136,9 @@ func detectEvent(inotify_object *watch_struct, wd uint32, path string, mask uint
 			noty(evName, "null", path, 1, 1)
 		}
 	}
-	if mask&event_type[6] != 0 {
+	if mask&eventType[6] != 0 {
 		totName := evName + path
-		if mask&file_type != 0 {
+		if mask&fileType != 0 {
 			fmt.Printf("Dir %v has been deleted\n", totName)
 			noty(evName, "null", path, 0, 1)
 		} else {
@@ -146,8 +146,8 @@ func detectEvent(inotify_object *watch_struct, wd uint32, path string, mask uint
 			noty(evName, "null", path, 1, 1)
 		}
 	}
-	if mask&event_type[2] != 0 {
-		if mask&file_type != 0 {
+	if mask&eventType[2] != 0 {
+		if mask&fileType != 0 {
 			fmt.Printf("New dir %v has been modified in %v\n", evName, path)
 			noty(evName, "null", path, 0, 2)
 		} else {
@@ -155,10 +155,10 @@ func detectEvent(inotify_object *watch_struct, wd uint32, path string, mask uint
 			noty(evName, "null", path, 1, 2)
 		}
 	}
-	if mask&event_type[3] != 0 {
-		_, ok := inotify_object.objects[wd]
+	if mask&eventType[3] != 0 {
+		_, ok := inotifyObject.objects[wd]
 		if ok {
-			if mask&file_type != 0 {
+			if mask&fileType != 0 {
 				fmt.Printf("Dir %v has been moved from %v\n", evName, path)
 				noty(evName, "null", path, 0, 3)
 			} else {
@@ -167,10 +167,10 @@ func detectEvent(inotify_object *watch_struct, wd uint32, path string, mask uint
 			}
 		}
 	}
-	if mask&event_type[4] != 0 {
-		_, ok := inotify_object.objects[wd]
+	if mask&eventType[4] != 0 {
+		_, ok := inotifyObject.objects[wd]
 		if ok {
-			if mask&file_type != 0 {
+			if mask&fileType != 0 {
 				fmt.Printf("Dir %v has been moved to %v\n", evName, path)
 				noty(evName, "null", path, 0, 4)
 			} else {
@@ -205,12 +205,12 @@ func noty(d string, d1 string, path string, types int, action int) {
 	notify.UnInit()
 }
 
-/* recursive walks through path, and add file/folder to watch_list*/
-func recList(dir string, wd *watch_struct) error {
+/* recursive walks through path, and add file/folder to watchList*/
+func recList(dir string, wd *watchStruct) error {
 
 	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		absPath, _ := filepath.Abs(path)
-		wd.add_watch(absPath)
+		wd.addWatch(absPath)
 		if info.IsDir() {
 			fmt.Printf("DIR: %v.   (Just added to watch list)\n", absPath)
 		} else {
@@ -227,11 +227,11 @@ func recList(dir string, wd *watch_struct) error {
 func main() {
 	dir := os.Args[1]
 
-	wd, err := create_watch()
+	wd, err := createWatch()
 	if err != nil {
 		fmt.Println(err)
 	}
-	wd.add_watch(dir)
+	wd.addWatch(dir)
 	err = recList(dir, wd)
 	if err != nil {
 		fmt.Println(err)
